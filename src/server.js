d5cbfa8776b2a86f7124b1581b472698c97a2b98
hellicopthecat <114468016+hellicopthecat@users.noreply.github.com>
2023-02-27 13:18:36 +0900

    create websocket server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket, {WebSocketServer} from "ws";
 
 const app = express();
 const port = 8000;
@@ -9,6 +11,12 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () =>
-  console.log(" ✅ Server is Listening localhost:8000 ✅");
-app.listen(port, handleListen);
+  console.log(`✅ Server is Listening http://localhost:${port} ✅`);
+// app.listen(port, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
+server.listen(port, handleListen);

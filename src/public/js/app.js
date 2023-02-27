bbd9f61cf93c67061f4d645c8342fcd7c9af8cbe
hellicopthecat <114468016+hellicopthecat@users.noreply.github.com>
2023-02-27 21:59:48 +0900

    message Theory

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,38 @@
-hello;
+const messageUl = document.querySelector("ul");
+const nicknameForm = document.querySelector("#nickname");
+const messageForm = document.querySelector("#message");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("connected to server");
+});
+socket.addEventListener("close", () => {
+  console.log("disconnected from server");
+});
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageUl.append(li);
+});
+
+function makeMessage(type, payload) {
+  const msg = {type, payload};
+  return JSON.stringify(msg);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+}
+function handlenickSubmit(event) {
+  event.preventDefault();
+  const input = nicknameForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+messageForm.addEventListener("submit", handleSubmit);
+nicknameForm.addEventListener("submit", handlenickSubmit);
